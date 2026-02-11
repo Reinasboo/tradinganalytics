ae5e0322b6f82bd105773bcfa144277cfc3b670c
Reinasboo <reinasboo@gmail.com>
2026-02-11 21:32:17 +0100

    Add files via upload

---
+++ providers.tsx
@@ -0,0 +1,13 @@
+"use client"
+import { ReactNode } from 'react'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+
+export default function Providers({ children }: { children: ReactNode }) {
+  return (
+    <>
+      {children}
+      <ToastContainer position="bottom-right" theme="dark" />
+    </>
+  )
+}
